6f1f18cde21d24bade72d630e0911048fe425887
Marcin Watras <marcinwatras@MacBook-Air-Marcin.local>
2024-02-28 21:56:09 +0100

    28.02 21:55 change code in persistance.xml and add class EntityManager to Main class

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -1,14 +1,22 @@
 package domain;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.swing.JFrame;
 
 public class Main {
 	public static void main(String [] args) {
 		
+		EntityManagerFactory entityManagerFactory= Persistence.createEntityManagerFactory("myToDoDataBase");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
 		LoginWindow loginWindow = new LoginWindow();
 		loginWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		loginWindow.setLocationRelativeTo(null);
 		loginWindow.setVisible(true);
+		
+		
 		
 		
 	}
